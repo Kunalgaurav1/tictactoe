b8bfaaad08b1263191b90bf51e8d376030b2069f
kunalgaurav <kunalgaurav1420@gmail.com>
2023-07-15 07:49:23 +0530

    modified eslint a gitignore file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,9 +8,18 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module' },
+
+  settings: { 
+    react: { version: '18.2' } 
+  },
+
+  plugins: [
+    'react-refresh'
+  ],
+  
   rules: {
     'react-refresh/only-export-components': [
       'warn',
